7451710cc2a057b4b5847c511695cacf28669830
Khanh Quang <khanhquang213@gmail.com>
2024-03-09 00:23:58 +0700

    update UI full

--- player.py
+++ player.py
@@ -33,8 +33,8 @@ class Player(pygame.sprite.Sprite):
 
       # stats
       self.stats = {'health': 100, 'energy': 60, 'attack': 10, 'magic': 4, 'speed': 5}
-      self.health = self.stats['health']
-      self.energy = self.stats['energy']
+      self.health = self.stats['health'] * 0.5
+      self.energy = self.stats['energy'] * 0.8
       self.exp = 123
       self.speed = self.stats['speed']
 
