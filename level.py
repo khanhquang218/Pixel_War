04cabf8f953073c17e7135e1708e98d3b954546b
Khanh Quang <khanhquang213@gmail.com>
2024-03-12 00:15:01 +0700

    update

--- level.py
+++ level.py
@@ -7,6 +7,7 @@ from support import *
 from random import choice
 from weapon import Weapon
 from ui import UI
+from enemy import Enemy
 
 class Level:
    def __init__(self):
@@ -61,7 +62,8 @@ class Level:
                            self.create_attack,
                            self.destroy_attack,
                            self.create_magic)
-
+                     else:
+                        Enemy('squid', (x,y), [self.visible_sprites])
    def create_attack(self):
       self.current_attack =  Weapon(self.player,[self.visible_sprites])
 
