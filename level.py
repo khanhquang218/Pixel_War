4181597c3655a8d36c93daa55bfc5827c0b5d65c
Khanh Quang <khanhquang213@gmail.com>
2024-03-16 00:10:47 +0700

    update particles

--- level.py
+++ level.py
@@ -4,11 +4,11 @@ from tile import Tile
 from player import Player
 from debug import debug
 from support import *
-from random import choice
+from random import choice, randint
 from weapon import Weapon
 from ui import UI
 from enemy import Enemy
-
+from particles import AnimationPlayer
 class Level:
    def __init__(self):
 
@@ -29,6 +29,9 @@ class Level:
 
       # user interface
       self.ui = UI()
+
+      # particles
+      self.animation_player = AnimationPlayer()
 
    def create_map(self):
       layouts = {
@@ -100,6 +103,10 @@ class Level:
             if collision_sprites:
                for target_sprite in collision_sprites:
                   if target_sprite.sprite_type == 'grass':
+                     pos = target_sprite.rect.center
+                     offset = pygame.math.Vector2(0,75)
+                     for leaf in range(randint(3,6)):
+                        self.animation_player.create_grass_particles(pos - offset, [self.visible_sprites])
                      target_sprite.kill()
                   else:
                      target_sprite.get_damage(self.player, attack_sprite.sprite_type)
@@ -109,6 +116,7 @@ class Level:
          self.player.health -= amount
          self.player.vulnerable = False
          self.player.hurt_time = pygame.time.get_ticks()
+         self.animation_player.create_particles(attack_type, self.player.rect.center, [self.visible_sprites])
 
    def run(self):
       # update and draw the game
